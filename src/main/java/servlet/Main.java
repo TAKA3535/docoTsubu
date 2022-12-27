d9b6612d7b9b8bce9d3f280f6341f6d8329f124b
taka <neithi.gensin@gmail.com~>
2022-12-27 15:51:49 +0900

    second commit

--- src/main/java/servlet/Main.java
+++ src/main/java/servlet/Main.java
@@ -21,32 +21,39 @@ import model.User;
 public class Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) 
+			throws ServletException, IOException {
+		
 		//つぶやきリストをアプリケーションスコープから取得
 		ServletContext application = this.getServletContext();
-		List<Mutter> mutterList = (List<Mutter>) application.getAttribute("mutterList");
-		//取得できなかった場合は、つぶやきリストを新規作成してアプリケーションスコープに保存
+		List<Mutter> mutterList =
+			(List<Mutter>) application.getAttribute("mutterList");
+		//取得できなかった場合は、つぶやきリストを新規作成して
+		//アプリケーションスコープに保存
 		if (mutterList == null) {
 			mutterList = new ArrayList<>();
-			application.setAttribute("mutterLiset", mutterList);
+			application.setAttribute("mutterList", mutterList);
 		}
 		
 		//ログインしているか確認するためセッションスコープからユーザー情報を取得
 		HttpSession session = request.getSession();
 		User loginUser = (User) session.getAttribute("loginUser");
-		
 		if (loginUser == null) {	//ログインしていない場合
 			//リダイレクト
 			response.sendRedirect("/docoTsubu/");
-		}else {	//ログイン済みの場合
+		} else {	//ログイン済みの場合
 			//フォワード
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+			RequestDispatcher dispatcher = 
+				request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
 			dispatcher.forward(request, response);
 		}
 	}
 	//コード10-14
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) 
+			throws ServletException, IOException {
+		
 		//リクエストパラメータをの取得
 		request.setCharacterEncoding("UTF-8");
 		String text = request.getParameter("text");
@@ -55,7 +62,8 @@ public class Main extends HttpServlet {
 		if (text != null && text.length() != 0) {
 			//アプリケーションスコープに保存されたつぶやきリストを取得
 			ServletContext application = this.getServletContext();
-			List<Mutter> mutterList = (List<Mutter>)application.getAttribute("mutterList");
+			List<Mutter> mutterList = 
+				(List<Mutter>) application.getAttribute("mutterList");
 			
 			//セッションスコープに保存されたユーザー情報を取得
 			HttpSession session = request.getSession();
@@ -68,13 +76,16 @@ public class Main extends HttpServlet {
 			
 			//アプリケーションスコープにつぶやきリストを保存
 			application.setAttribute("mutterList", mutterList);
-		}else {	//コード10-16
+		} else {	//コード10-16
 			//エラーメッセージをリクエストスコープに保存
 			request.setAttribute("errorMsg", "つぶやきが入力されていません");
 		}
 		
+		
 		//メイン画面にフォワード
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+		RequestDispatcher dispatcher = 
+			request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+		
 		dispatcher.forward(request, response);
 	}
 }
